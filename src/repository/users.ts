d4f12ef4a3d126bcfd4c0289f1c396a3f3fa4f0f
edwinwalela <edwinwalela@gmail.com>
2022-06-25 13:03:13 +0300

    user repo

---
+++ src/repository/users.ts
@@ -0,0 +1,19 @@
+import db from '../config/db';
+import User from '../models/users';
+import { QueryResult } from 'pg';
+
+const getUsers = async (): Promise<User[]> => {
+	var users: User[] = [];
+	let result: QueryResult<User>;
+	try {
+		result = await db.query('SELECT * FROM users');
+	} catch (error) {
+		throw new Error(`Failed to query users: ${error}`);
+	}
+
+	result.rows.forEach((user) => {
+		users.push(user);
+	});
+
+	return users;
+};
